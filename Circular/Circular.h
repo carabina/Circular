f91e58e05c4b046964e9239fcf2f085875148ea0
Mahdy <ahmed.mahdy@vodafone.com>
2018-10-29 11:44:50 +0200

    adding files

---
+++ Circular/Circular.h
@@ -0,0 +1,17 @@
+//
+//  Circular.h
+//  Circular
+//
+//  Created by ahmed mahdy on 10/29/18.
+//  Copyright © 2018 ahmed mahdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Circular.
+FOUNDATION_EXPORT double CircularVersionNumber;
+
+//! Project version string for Circular.
+FOUNDATION_EXPORT const unsigned char CircularVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Circular/PublicHeader.h>
